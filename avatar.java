c13354cc4633ed632f74dd03af12e88828b63498
Erik Englund <erikw.englund@gmail.com>
2014-10-29 15:09:03 +0100

    added first files and gitignore

---
+++ avatar.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class avatar{
+    private String name;
+    private ArrayList<String> unfinishedCourses = new ArrayList<String>();
+    private ArrayList<String> finishedCourses = new ArrayList<String>();
+    private int HP = 60;
+    public void setName(String inputName){
+	name = inputName;
+    }
+    public String getName(){
+	return name;
+    }
+    public void addFinishedCourse(String courseName){
+	finishedCourses.add(courseName);
+    }
+    public void addUnfinishedCourse(String course){
+	unfinishedCourses.add(course);
+    }
+    public void updateHP(int points){
+	HP += points;
+    }
+    public void printList(){
+	int length = finishedCourses.size();
+	System.out.print("You have completed the following courses: ");
+	for (int i = 0;i<(length-1);i++){
+	    System.out.printf("%s, ", finishedCourses.get(i));
+	}
+	System.out.printf("%s\n", finishedCourses.get(length-1));
+    }
+    //public void finishCourse, hanterar metoder ovan och examinerar kurs
+}
---
+++ main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class main
+{
+    public static void main(String args[])
+    {
+	avatar Erik = new avatar();
+	System.out.println("Enter player name: ");
+	Scanner name = new Scanner(System.in);
+	String namn1 = name.nextLine();
+	Erik.setName(namn1);
+	System.out.printf("Hi, %s! Welcome to P0ll4x!!\n", Erik.getName());
+	Erik.addFinishedCourse("Bokvetenskap 101");
+	Erik.addFinishedCourse("Datakomm. 301");
+	Erik.printList();
+    }
+}
